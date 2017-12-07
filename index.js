02b74360bf97cd3cb6d41675af91f21882a4fa92
Deepak54321 <dmishra@yamaha-motor-india.com>
2017-12-06 22:39:31 -0800

    validation4

--- index.js
+++ index.js
@@ -87,104 +87,21 @@ if(req.body.result.action=='ask')
   
   }
   
-    if(req.body.result.action=='email-val')
-  {
-       var result=req.body.result;
-            var context=result.contexts[0];
-            var Email=context.parameters.ProductEnquiryEmail;
-            var regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-             //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-      console.log("%s",Email);
-      var EmailCheck= Email.substring(0, Email.indexOf(' com'));
-      EmailCheck=EmailCheck+'.com';
-      var check=regex.test(EmailCheck);
-      console.log(" check status %s",check);
-      if(check)
-      {
-          var message='please share your pincode';
-            res.status(200).json({
-           source: 'webhook',
-           //speech: message,
-           //displayText: message,
-            'messages': 
-              [{
-                   'type':0,
-                   'speech':message
-               }]
-            })
-      }
-      else
-      {
-           res.status(200).json({
-                            source: 'webhook',
-                            speech: 'Invalid email Please enter again',
-                            displayText: 'Invalid email Please enter again',
-                            "followupEvent":{
-                        "name":"re_email",
-                            "data":
-                            {
-                                "ProductEnquiryEmail":""
-                            }
-                        }
-        
-                                })
-      }
+  //Test Drive
   
-  }
-    
-        if(req.body.result.action=='pin-val')
+  if(req.body.result.action=='testask')
   {
        var result=req.body.result;
             var context=result.contexts[0];
-            var Pin=context.parameters.pincode;
-            var regex = /^([1-9])([0-9]){5}$/;
-             //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-      console.log("%s",Pin);
-      if(regex.test(Pin))
-      {
-          var message='please share your chasis Number';
-            res.status(200).json({
-           source: 'webhook',
-           //speech: message,
-           //displayText: message,
-            'messages': 
-              [{
-                   'type':0,
-                   'speech':message
-               }]
-            })
-      }
-      else
-      {
-           res.status(200).json({
-                            source: 'webhook',
-                            speech: 'Invalid email Please enter again',
-                            displayText: 'Invalid email Please enter again',
-                            "followupEvent":{
-                        "name":"re_pin",
-                            "data":
-                            {
-                                "pincode":""
-                            }
-                        }
-        
-                                })
-      }
-  
-  }
-    
-    //Test Drive
-     
-    if(req.body.result.action=='test-phone')
-  {
-       var result=req.body.result;
-            var context=result.contexts[0];
-            var PhoneNumber=context.parameters.phonenumber;
+            var PhoneNumber=context.parameters.testphonenumber;
+            var Name=context.parameters.TestName;
+            var Email=context.parameters.TestEmail;
+            var pincode=context.parameters.TestPincode;
             var pattern = /^\d{10}$/;
       console.log("%s",PhoneNumber);
       if(pattern.test(PhoneNumber))
       {
-          var message='please share your email';
+          var message='Thanks for sharing your details now you can proceed further.';
             res.status(200).json({
            source: 'webhook',
            //speech: message,
@@ -193,7 +110,11 @@ if(req.body.result.action=='ask')
               [{
                    'type':0,
                    'speech':message
-               }]
+               },
+                                {'title': 'Please Select product type',
+                                'replies': ['Motorcycles','Scooter'],
+                                'type': 2}
+               ]
             })
       }
       else
@@ -203,96 +124,13 @@ if(req.body.result.action=='ask')
                             speech: 'Invalid phone Number Please enter again',
                             displayText: 'Invalid phone Number Please enter again',
                             "followupEvent":{
-                        "name":"testre_phone",
+                        "name":"testre_ask",
                             "data":
                             {
-                                "phonenumber":""
-                            }
-                        }
-        
-                                })
-      }
-  
-  }
-  
-    if(req.body.result.action=='testemail-val')
-  {
-       var result=req.body.result;
-            var context=result.contexts[0];
-            var Email=context.parameters.email;
-            var regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-             //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-      console.log("%s",Email);
-      var EmailCheck= Email.substring(0, Email.indexOf(' com'));
-      EmailCheck=EmailCheck+'.com';
-      var check=regex.test(EmailCheck);
-      console.log(" check status %s",check);
-      if(check)
-      {
-          var message='please share your pincode';
-            res.status(200).json({
-           source: 'webhook',
-           //speech: message,
-           //displayText: message,
-            'messages': 
-              [{
-                   'type':0,
-                   'speech':message
-               }]
-            })
-      }
-      else
-      {
-           res.status(200).json({
-                            source: 'webhook',
-                            speech: 'Invalid email Please enter again',
-                            displayText: 'Invalid email Please enter again',
-                            "followupEvent":{
-                        "name":"testre_email",
-                            "data":
-                            {
-                                "email":""
-                            }
-                        }
-        
-                                })
-      }
-  
-  }
-    
-        if(req.body.result.action=='testpin-val')
-  {
-       var result=req.body.result;
-            var context=result.contexts[0];
-            var Pin=context.parameters.pincode;
-            var regex = /^([1-9])([0-9]){5}$/;
-             //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-      console.log("%s",Pin);
-      if(regex.test(Pin))
-      {
-          var message='please share your chasis Number';
-            res.status(200).json({
-           source: 'webhook',
-           //speech: message,
-           //displayText: message,
-            'messages': 
-              [{
-                   'type':0,
-                   'speech':message
-               }]
-            })
-      }
-      else
-      {
-           res.status(200).json({
-                            source: 'webhook',
-                            speech: 'Invalid email Please enter again',
-                            displayText: 'Invalid email Please enter again',
-                            "followupEvent":{
-                        "name":"testre_pin",
-                            "data":
-                            {
-                                "pincode":""
+                                "TestName":Name,
+                                "TestEmail":Email,
+                                "TestPincode":pincode,
+                                 "testphonenumber":""
                             }
                         }
         
@@ -302,16 +140,20 @@ if(req.body.result.action=='ask')
   }
   
   //Complaint
-    if(req.body.result.action=='Complaint-phone')
+  
+  if(req.body.result.action=='compask')
   {
        var result=req.body.result;
             var context=result.contexts[0];
-            var PhoneNumber=context.parameters.phonenumber;
+            var PhoneNumber=context.parameters.complaintphonenumber;
+            var Name=context.parameters.complaintname;
+            var Email=context.parameters.ComplaintEmail;
+           // var pincode=context.parameters.pincode
             var pattern = /^\d{10}$/;
       console.log("%s",PhoneNumber);
       if(pattern.test(PhoneNumber))
       {
-          var message='please share your email';
+          var message='Thanks for sharing your details now you can proceed further.';
             res.status(200).json({
            source: 'webhook',
            //speech: message,
@@ -320,7 +162,11 @@ if(req.body.result.action=='ask')
               [{
                    'type':0,
                    'speech':message
-               }]
+               },
+                                {'title': 'Please Select product type',
+                                'replies': ['Motorcycles','Scooter'],
+                                'type': 2}
+               ]
             })
       }
       else
@@ -330,10 +176,12 @@ if(req.body.result.action=='ask')
                             speech: 'Invalid phone Number Please enter again',
                             displayText: 'Invalid phone Number Please enter again',
                             "followupEvent":{
-                        "name":"Complaintre_phone",
+                        "name":"re_ask",
                             "data":
                             {
-                                "phonenumber":""
+                                "complaintname":Name,
+                                "ComplaintEmail":Email,
+                                 "complaintphonenumber":""
                             }
                         }
         
@@ -341,93 +189,7 @@ if(req.body.result.action=='ask')
       }
   
   }
-  
-    if(req.body.result.action=='Complaintemail-val')
-  {
-       var result=req.body.result;
-            var context=result.contexts[0];
-            var Email=context.parameters.email;
-            var regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-             //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-      console.log("%s",Email);
-      var EmailCheck= Email.substring(0, Email.indexOf(' com'));
-      EmailCheck=EmailCheck+'.com';
-      var check=regex.test(EmailCheck);
-      console.log(" check status %s",check);
-      if(check)
-      {
-          var message='please share your pincode';
-            res.status(200).json({
-           source: 'webhook',
-           //speech: message,
-           //displayText: message,
-            'messages': 
-              [{
-                   'type':0,
-                   'speech':message
-               }]
-            })
-      }
-      else
-      {
-           res.status(200).json({
-                            source: 'webhook',
-                            speech: 'Invalid email Please enter again',
-                            displayText: 'Invalid email Please enter again',
-                            "followupEvent":{
-                        "name":"Complaintre_email",
-                            "data":
-                            {
-                                "email":""
-                            }
-                        }
-        
-                                })
-      }
-  
-  }
-    
-        if(req.body.result.action=='pin-val')
-  {
-       var result=req.body.result;
-            var context=result.contexts[0];
-            var Pin=context.parameters.pincode;
-            var regex = /^([1-9])([0-9]){5}$/;
-             //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-      console.log("%s",Pin);
-      if(regex.test(Pin))
-      {
-          var message='please share your chasis Number';
-            res.status(200).json({
-           source: 'webhook',
-           //speech: message,
-           //displayText: message,
-            'messages': 
-              [{
-                   'type':0,
-                   'speech':message
-               }]
-            })
-      }
-      else
-      {
-           res.status(200).json({
-                            source: 'webhook',
-                            speech: 'Invalid email Please enter again',
-                            displayText: 'Invalid email Please enter again',
-                            "followupEvent":{
-                        "name":"re_pin",
-                            "data":
-                            {
-                                "pincode":""
-                            }
-                        }
-        
-                                })
-      }
-  
-  }
-
+ 
 
 
    if(req.body.result.action=='Priceapi')
